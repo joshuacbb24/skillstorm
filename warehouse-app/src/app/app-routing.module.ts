a797130079049402627a599979b75feb51c7c637
joshua <joshu@DESKTOP-LN7DOPJ>
2022-07-15 09:52:55 -0400

    routing

--- warehouse-app/src/app/app-routing.module.ts
+++ warehouse-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeContentComponent } from './home-content/home-content.component';
+import { WarehouseListComponent } from './warehouse-list/warehouse-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeContentComponent},
+  {path: 'warehouses', component: WarehouseListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
