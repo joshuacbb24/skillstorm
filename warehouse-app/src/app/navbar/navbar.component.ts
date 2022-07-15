ed44d872a914d498fa17461da74d23c47905ee56
joshua <joshu@DESKTOP-LN7DOPJ>
2022-07-14 23:13:58 -0400

    main content started

--- warehouse-app/src/app/navbar/navbar.component.ts
+++ warehouse-app/src/app/navbar/navbar.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  toggleSidebar() :void{
+    
+    let sidebarElement = document.querySelector(".sidebar");
+    let navElement = document.querySelector("nav");
+    let dashElement = document.querySelector(".home-section");
+    
+    console.log(navElement)
+
+    sidebarElement ? sidebarElement.classList.toggle("active") : null;
+    dashElement ? dashElement.classList.toggle("active") : null;
+    navElement ? navElement.classList.toggle("active") : null;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
