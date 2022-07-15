bdb9370d51dafc945bc8273c4c5b67f57fb71c06
joshua <joshu@DESKTOP-LN7DOPJ>
2022-07-15 10:28:10 -0400

    redirect

--- warehouse-app/src/app/app-routing.module.ts
+++ warehouse-app/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { InventoryListComponent } from './inventory-list/inventory-list.componen
 import { WarehouseListComponent } from './warehouse-list/warehouse-list.component';
 
 const routes: Routes = [
-  {path: '', component: HomeContentComponent},
+  {path: '', redirectTo: 'home', pathMatch:'full'},
   {path: 'home', component: HomeContentComponent},
   {path: 'warehouses', component: WarehouseListComponent},
   {path: 'inventory', component: InventoryListComponent},
